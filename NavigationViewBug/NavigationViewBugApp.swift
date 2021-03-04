27e0a3dddab1301b3583151b39850909b98c5c74
Damiaan Dufaux <damiaan@dpro.be>
2021-03-04 10:03:09 +0100

    Initial Commit

---
+++ NavigationViewBug/NavigationViewBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewBugApp.swift
+//  NavigationViewBug
+//
+//  Created by Damiaan on 04/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct NavigationViewBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
