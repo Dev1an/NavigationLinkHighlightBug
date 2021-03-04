151a256c68162166adeb776858245fe2ea6467aa
Damiaan Dufaux <damiaan@dpro.be>
2021-03-04 10:28:30 +0100

    Add NavigationView

--- NavigationViewBug/ContentView.swift
+++ NavigationViewBug/ContentView.swift
@@ -9,9 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+		NavigationView {
+			List {
+				NavigationLink("Local", destination: localDevices.navigationTitle("Local"))
+				NavigationLink("Remote", destination: remoteDevices.navigationTitle("Remote"))
+				ForEach(0..<30) { Text("Location \($0)") }
+			}.navigationTitle("Locations")
+		}
     }
+
+	var localDevices: some View {
+		List {
+			Text("iPhone")
+			Text("iPad")
+		}
+	}
+
+	var remoteDevices: some View {
+		List {
+			Text("Mac Pro")
+			Text("iMac Pro")
+			Text("Airpods")
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
